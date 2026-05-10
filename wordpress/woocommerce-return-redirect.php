2acc2effd55b1a31fb03917d212ffc8c3be78ef0
shopowner58 <frederik@flagrant.be>
2026-05-10 21:50:40 +0200

    Allow Astro return redirects from WooCommerce

--- wordpress/woocommerce-return-redirect.php
+++ wordpress/woocommerce-return-redirect.php
@@ -5,7 +5,17 @@
  * Add this to a small site plugin or the active WooCommerce checkout theme's
  * functions.php. It lets Astro links append ?return_to=https://flipsight.be/shop
  * after WooCommerce adds the product to the cart, while blocking open redirects.
+ *
+ * The allowed_redirect_hosts filter is required because WooCommerce uses
+ * wp_safe_redirect(). Without this, WordPress falls back to wp-admin for
+ * cross-domain redirects and visitors see the login screen.
  */
+add_filter('allowed_redirect_hosts', function ($hosts) {
+    $hosts[] = 'flipsight.be';
+    $hosts[] = 'www.flipsight.be';
+    return array_values(array_unique($hosts));
+});
+
 add_filter('woocommerce_add_to_cart_redirect', function ($url) {
     if (empty($_REQUEST['return_to'])) {
         return $url;
